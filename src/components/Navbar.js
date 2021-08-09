46fba5c4d918613290620ddc8b791c5a52266b89
Sepehr <sep.farahani@gmail.com>
2021-08-09 16:33:19 -0700

    Gatsby Tutorial Part 2

---
+++ src/components/Navbar.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
+      <h1 className="pl-8 text-red-500">Navbar</h1>
+    </div>
+  );
+};
+
+export default Navbar;
