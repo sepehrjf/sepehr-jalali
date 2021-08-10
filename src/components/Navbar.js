c381f60eefeaf91048de1047c67921ffaeee2095
Sepehr <sep.farahani@gmail.com>
2021-08-10 16:32:18 -0700

    Just Started

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,4 @@
-import * as React from "react";
+import React from "react";
 import { Link } from "gatsby";
 
 const Navbar = () => {
@@ -6,6 +6,18 @@ const Navbar = () => {
     <header className="text-gray-600 body-font">
       <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
         <a className='flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0"'>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            stroke="currentColor"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            className="w-10 h-10 text-white p-2 bg-indigo-500 rounded-full"
+            viewBox="0 0 24 24"
+          >
+            <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
+          </svg>
           <span className="ml-3 text-xl">Sepehr Jalali</span>
         </a>
         <nav className="md:ml-auto flex flex-wrap items-center text-base justify-center">
