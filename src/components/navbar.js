dea7be75b2a16d5b38db5482ab1c8646962e1474
Sepehr <sep.farahani@gmail.com>
2021-08-11 15:36:50 -0700

    Updated Navbar Code

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -6,28 +6,17 @@ const Navbar = () => {
     <header>
       <div
         className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center"
-        style={{ fontFamily: "Mulish, sans-serif" }}
+        style={{ fontFamily: "Mulish, sans-serif", letterSpacing: "0.13em" }}
       >
         <Link
           to="/"
-          className="flex items-center mb-4 md:mb-0"
-          style={{
-            fontSize: "1rem",
-            fontWeight: "600",
-            textTransform: "lowercase",
-            letterSpacing: "0.13em",
-          }}
+          className="flex items-center text-base font-semibold mb-4 md:mb-0 lowercase"
         >
           <span className="ml-3 text-xl">sepehrjalali</span>
         </Link>
         <nav
-          className="md:ml-auto flex flex-wrap items-center text-base justify-center"
-          style={{
-            fontSize: "0.9357rem",
-            fontWeight: "400",
-            textTransform: "lowercase",
-            letterSpacing: "0.13em",
-          }}
+          className="md:ml-auto flex flex-wrap items-center text-base font-normal justify-center lowercase"
+          style={{ fontSize: "0.9357rem" }}
         >
           <Link to="/about" className="mr-5 hover:text-gray-900">
             about
