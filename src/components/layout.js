6118e30413fd404c6a15bfe4ffb8e3807e445df0
Sepehr <sep.farahani@gmail.com>
2021-08-11 00:09:13 -0700

    Just Started

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,12 +1,13 @@
 import React from "react";
 //import Navbar from "./navbar";
+import Header from "./header";
 import Footer from "./footer";
 import { container } from "./layout.module.css";
 
 const Layout = ({ pageTitle, children }) => {
   return (
     <div className={container}>
-      {/* <Navbar /> */}
+      <Header />
       {children}
       <Footer />
     </div>
