9f3394d5c4d8cf63517346003c2d4749d1e7c04d
Sepehr <sep.farahani@gmail.com>
2021-08-11 14:25:23 -0700

    Applied Hero Fonts

--- src/components/hero.js
+++ src/components/hero.js
@@ -6,20 +6,32 @@ const Hero = () => {
     <section className={container}>
       <div className="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
         <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
-          <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">
+          <p
+            className="sm:text-sm text-xs lowercase mb-4 relative text-gray-900"
+            style={{
+              fontFamily: "Mulish, sans-serif",
+              letterSpacing: "0.16em",
+            }}
+          >
             hello, world!
+          </p>
+          <h1
+            className="sm:text-7xl text-5xl font-bold mb-4 relative text-gray-900"
+            style={{
+              fontFamily: "Playfair Display, serif",
+            }}
+          >
+            I am a Software Engineer
           </h1>
-          <p className="mb-8 leading-relaxed">
-            Copper mug try-hard pitchfork pour-over freegan heirloom neutra air
-            plant cold-pressed tacos poke beard tote bag. Heirloom echo park
-            mlkshk tote bag selvage hot chicken authentic tumeric truffaut
-            hexagon try-hard chambray.
+          <p
+            className="mb-8 leading-relaxed font-normal text-base mb-2"
+            style={{ fontFamily: "Mulish, sans-serif" }}
+          >
+            I am passionate about problem solving, web development, and enjoy
+            making the Internet a more beautiful place.
           </p>
           <div className="flex justify-center">
             <button className="inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">
-              Button
-            </button>
-            <button className="ml-4 inline-flex text-gray-700 bg-gray-100 border-0 py-2 px-6 focus:outline-none hover:bg-gray-200 rounded text-lg">
               Button
             </button>
           </div>
