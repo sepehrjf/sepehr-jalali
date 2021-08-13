2443e0466e0f47c82c674634bb127159b6e66356
Sepehr <sep.farahani@gmail.com>
2021-08-13 01:15:39 -0700

    Started Project Card Design

---
+++ src/components/projectCard.js
@@ -0,0 +1,77 @@
+import React from "react";
+import ButtonYellow from "./buttonYellow";
+
+const ProjectCard = () => {
+  return (
+    <section className="text-gray-600">
+      <div className="container px-5 py-24 mx-auto">
+        <div className="flex flex-wrap -mx-4 -mb-10 text-left">
+          <div className="sm:w-1/2 mb-10 border border-solid border-transparent shadow-lg">
+            <div className="h-64 overflow-hidden">
+              <img
+                alt="content"
+                className="object-cover object-center h-full w-full"
+                src="https://dummyimage.com/1201x501"
+              />
+            </div>
+            <div className="p-8">
+              <h2
+                className="font-extralight text-gray-900 mt-6 mb-1.5"
+                style={{
+                  fontFamily: "Playfair Display, serif",
+                  fontSize: "2rem",
+                  lineHeight: "1",
+                }}
+              >
+                Taillow
+              </h2>
+              <p
+                className="leading-relaxed text-base font-normal"
+                style={{
+                  fontFamily: "Mulish, sans-serif",
+                }}
+              >
+                Williamsburg occupy sustainable snackwave gochujang. Pinterest
+                cornhole brunch, slow-carb neutra irony.
+              </p>
+              <ButtonYellow name="read more" />
+            </div>
+          </div>
+          <div className="sm:w-1/2 mb-10 border border-solid border-transparent shadow-lg">
+            <div className="h-64 overflow-hidden">
+              <img
+                alt="content"
+                class="object-cover object-center h-full w-full"
+                src="https://dummyimage.com/1202x502"
+              />
+            </div>
+            <div className="p-7">
+              <h2
+                className="font-extralight text-gray-900 mt-6 mb-1.5"
+                style={{
+                  fontFamily: "Playfair Display, serif",
+                  fontSize: "2rem",
+                  lineHeight: "1",
+                }}
+              >
+                The Catalyzer
+              </h2>
+              <p
+                className="leading-relaxed text-base font-normal"
+                style={{
+                  fontFamily: "Mulish, sans-serif",
+                }}
+              >
+                Williamsburg occupy sustainable snackwave gochujang. Pinterest
+                cornhole brunch, slow-carb neutra irony
+              </p>
+              <ButtonYellow name="read more" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default ProjectCard;
--- src/pages/projects.js
+++ src/pages/projects.js
@@ -1,6 +1,7 @@
 import * as React from "react";
-import Hero from "../components/hero";
 import Layout from "../components/layout";
+import Hero from "../components/hero";
+import ProjectCard from "../components/projectCard";
 
 const ProjectsPage = () => {
   return (
@@ -9,6 +10,7 @@ const ProjectsPage = () => {
         title="Lifelong Learner"
         description="Every project is crafted with care and servers as a learning experience."
       />
+      <ProjectCard />
     </Layout>
   );
 };
