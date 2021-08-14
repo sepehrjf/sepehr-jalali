2f090a70e785df6f24a0cc653ebfa828643b7f7e
Sepehr <sep.farahani@gmail.com>
2021-08-13 19:09:28 -0700

    Fixed Project Card Width

--- src/components/projectCard.js
+++ src/components/projectCard.js
@@ -6,7 +6,7 @@ const ProjectCard = () => {
     <section className="text-gray-600">
       <div className="container px-5 py-24 mx-auto">
         <div className="flex flex-wrap justify-center -mx-2.5 -mb-10 text-left">
-          <div className="sm:w-1/4 md:w-2/6 lg:w-2/5 mb-10 border border-solid border-transparent shadow-lg mx-2.5">
+          <div className="sm:w-2/5 mb-10 border border-solid border-transparent shadow-lg mx-2.5">
             <div className="h-64 overflow-hidden">
               <img
                 alt="content"
@@ -37,7 +37,7 @@ const ProjectCard = () => {
               <ButtonYellow name="read more" />
             </div>
           </div>
-          <div className="sm:w-1/4 md:w-2/6 lg:w-2/5 mb-10 border border-solid border-transparent shadow-lg mx-2.5">
+          <div className="sm:w-2/5 mb-10 border border-solid border-transparent shadow-lg mx-2.5">
             <div className="h-64 overflow-hidden">
               <img
                 alt="content"
