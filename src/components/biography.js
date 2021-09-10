fd5c949922c880de7bf3e61fd973e68a54342c3d
Sepehr <sep.farahani@gmail.com>
2021-09-10 16:53:52 -0700

    Test Gatsby

--- src/components/biography.js
+++ src/components/biography.js
@@ -3,12 +3,12 @@ import { tile } from "./tile.module.css";
 
 function Biography(props) {
   return (
-    <div className="container flex flex-col flex-grow flex-auto text-gray-800 mx-auto pt-2 pb-4">
+    <div className="container flex flex-col flex-grow flex-auto mx-auto pt-2 pb-4">
       <div className="px-5 sm:px-10 md:px-24 lg:px-48">
         <div className="flex mb-4 relative">
           <div className={tile}></div>
           <div
-            className="question text-xs sm:text-sm lowercase"
+            className="question text-xs sm:text-sm lowercase text-gray-800"
             style={{
               fontFamily: "Mulish, sans-serif",
               letterSpacing: "0.13em",
